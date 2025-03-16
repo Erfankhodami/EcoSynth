a54c6e67e93e97c834beed025edb662dd15e218f
Erfankhodami <khodami.erfan@gmail.com>
2025-03-17 01:36:37 +0330

    added player basic movment and jump system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+    private float movingControll;
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private float horizontalSlipperiness=100;
+    [SerializeField] private float jumpingPower;
+    private int jumpCount = 2;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        //basic player movement
+        movingControll = Input.GetAxis("Horizontal");
+        //drag system
+        playerRB.AddForce(Vector2.right*movingControll*movingSpeed*Time.deltaTime,ForceMode2D.Force);
+        Vector3 hVelocity = playerRB.velocity;
+        hVelocity.x *= Time.deltaTime*horizontalSlipperiness;
+        playerRB.velocity = hVelocity;
+        
+        if (Input.GetKeyDown(KeyCode.Space)&& jumpCount!=0)
+        {
+            //jumping force system
+            Vector3 vVelocity = playerRB.velocity;
+            vVelocity.y = jumpingPower;
+            playerRB.velocity = vVelocity;
+            jumpCount--;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            jumpCount = 2;
+        }
+    }
+}
